1fc9afca20e6f8f2cd451cb9a19c25e99746c7a8
cuddlyfiesta <keith@laughingsquirrel.net>
2023-10-13 13:10:59 -0400

    Final changes

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,33 +1,53 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext';
-import '../Currency.css'; // Import a custom CSS file for styling
+import React, { useState, useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import { Dropdown } from "react-bootstrap";
 
 const Currency = () => {
-    const { dispatch, currency } = useContext(AppContext);
-
-    const handleCurrencyChange = (event) => {
-        const newCurrency = event.target.value;
-        dispatch({ type: 'CHG_CURRENCY', payload: newCurrency });
-    };
-
-    return (
-        <div className='col-sm'>
-            <div className={`alert alert-success ${currency === '' ? '' : 'open'}`}>
-                <label htmlFor="currencyDropdown">Currency ({currency})</label>
-                <select
-                    id="currencyDropdown"
-                    value={currency}
-                    onChange={handleCurrencyChange}
-                    className={`form-control custom-dropdown ${currency === '' ? '' : 'btn-success'}`} // Apply Bootstrap classes
-                >
-                    <option value="$">$ Dollar</option>
-                    <option value="£">£ Pound</option>
-                    <option value="€">€ Euro</option>
-                    <option value="₹">₹ Rupee</option>
-                </select>
-            </div>
-        </div>
-    );
+  const CURRENCY_LIST = [
+    {
+      symbol: "£",
+      name: "Pound",
+    },
+    {
+      symbol: "$",
+      name: "Dollar",
+    },
+    {
+      symbol: "₹",
+      name: "Ruppee",
+    },
+    {
+      symbol: "€",
+      name: "Euro",
+    },
+  ];
+  const { dispatch } = useContext(AppContext);
+  const [selectedCurrency, setSelectedCurrency] = useState({
+    symbol: "£",
+    name: "Pound",
+  });
+  const changeCurrency = (currency) => {
+    setSelectedCurrency(currency);
+    dispatch({
+      type: "CHG_CURRENCY",
+      payload: currency.symbol,
+    });
+  };
+  return (
+    <Dropdown>
+      <Dropdown.Toggle variant="success" className="alert alert-success text-white">
+        Currency ({selectedCurrency.symbol} {selectedCurrency.name})
+      </Dropdown.Toggle>
+      <Dropdown.Menu className="alert alert-success border border-success bg-success text-black">
+        {CURRENCY_LIST.map((currency) => (
+          <Dropdown.Item onClick={() => changeCurrency(currency)}>
+            {currency.symbol} {currency.name}
+          </Dropdown.Item>
+        ))}
+      </Dropdown.Menu>
+    </Dropdown>
+    
+  );
 };
 
 export default Currency;
